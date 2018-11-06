4e305b01b9d95d81e493a4d6617cf83cd50e8307
tswysy <tswysy@gmail.com>
2018-11-06 17:16:18 +0800

    03.竞标结构及方法

--- contracts/EcommerceStore.sol
+++ contracts/EcommerceStore.sol
@@ -18,6 +18,7 @@ contract EcommerceStore {
         uint secondHighestBid;  //次高价
         ProductStatus status;  //产品销售状态
         ProductCondition condition; //产品状态
+        mapping(address => mapping(bytes32 => Bid)) bids;
     }
 
     enum ProductStatus {Open, Sold, Unsold}
@@ -62,5 +63,47 @@ contract EcommerceStore {
         Product memory product = stores[owner][_productId];
         return (product.id, product.name, product.category, product.imageLink, product.descLink, product.auctionStartTime, product.auctionEndTime, product.startPrice, uint(product.status));
     }
+
+
+    //竞标的结构
+    struct Bid{
+        uint productId;
+        uint price;
+        bool isRevealed;
+        address bidder;
+    }
+
+    //竞标函数
+    function bid(uint _productId,bytes32 _bidHash)public payable{
+        //找到商品
+        Product storage product = stores[productIdToOwner[_productId]][_productId];
+        //竞拍总人数加一
+        product.totalBids++;
+        //构建竞标结构体
+        Bid memory bidLocal = Bid(_productId,msg.value,false,msg.sender);
+        //添加
+        product.bids[msg.sender][_bidHash] = bidLocal;
+    }
+
+    //生成哈希函数
+    function makeBidHash(string _realAmount,string _secret)public pure returns (bytes32){
+        return sha3(_realAmount,_secret);
+    }
+
+    //返回竞标
+    function getBidById(uint _productId,byte32 _bidId)public view returns(uint, uint, bool, address){
+        Product storage product = stores[productIdToOwner[_productId]][_productId];
+
+        Bid memory bid = product.bids[msg.sender][_bidId];
+
+        return (bid.productId,bid.price,bid.isRevealed,bid.bidder);
+    }
+
+    //返回当前金额
+    function getBalance()public view returns (uint){
+        return this.balance;
+    }
+
+
 
 }
