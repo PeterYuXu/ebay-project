6ff34355417caed57e12fc136cf068dd09d32c4f
tswysy <tswysy@gmail.com>
2018-11-14 16:21:10 +0800

    08-调用商城合约成功

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,47 +1,57 @@
 // Import the page's CSS. Webpack will know what to do with it.
 import '../styles/app.css'
-
 // Import libraries we need.
 import { default as Web3 } from 'web3'
 import { default as contract } from 'truffle-contract'
-
 // Import our contract artifacts and turn them into usable abstractions.
-import metaCoinArtifact from '../../build/contracts/MetaCoin.json'
+import ecommerceStore from '../../build/contracts/EcommerceStore.json'
 
 // MetaCoin is our usable abstraction, which we'll use through the code below.
-const MetaCoin = contract(metaCoinArtifact)
+const ecommerceStoreContract = contract(ecommerceStore)
+let ecommerceStoreInstance
 
 const App = {
-  start: function () {
-    console.log('Hello')
-  }
+    start: async function () {
+        console.log('Hello')
+        ecommerceStoreContract.setProvider(window.web3.currentProvider)
+        ecommerceStoreInstance = await ecommerceStoreContract.deployed()
+        let res = await ecommerceStoreInstance.addProductToStore(
+            '衣服', '服装', 'imagelink111', 'descLink222', 2018, 2019, 10, 0, {
+                from: window.web3.eth.accounts[0]
+            })
+
+        console.log('res:', res)
+
+        res = await ecommerceStoreInstance.getProductById(1)
+        console.log('res product info:', res)
+    }
 }
 
 window.App = App
 
 window.addEventListener('load', function () {
-  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-  if (typeof web3 !== 'undefined') {
-    console.warn(
-      'Using web3 detected from external source.' +
-      ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
-      ' ensure you\'ve configured that source properly.' +
-      ' If using MetaMask, see the following link.' +
-      ' Feel free to delete this warning. :)' +
-      ' http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // Use Mist/MetaMask's provider
-    window.web3 = new Web3(web3.currentProvider)
-  } else {
-    console.warn(
-      'No web3 detected. Falling back to http://127.0.0.1:9545.' +
-      ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
-      ' Consider switching to Metamask for development.' +
-      ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
-    )
-    // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
-    window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
-  }
+    // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+    if (typeof web3 !== 'undefined') {
+        console.warn(
+            'Using web3 detected from external source.' +
+            ' If you find that your accounts don\'t appear or you have 0 MetaCoin,' +
+            ' ensure you\'ve configured that source properly.' +
+            ' If using MetaMask, see the following link.' +
+            ' Feel free to delete this warning. :)' +
+            ' http://truffleframework.com/tutorials/truffle-and-metamask'
+        )
+        // Use Mist/MetaMask's provider
+        window.web3 = new Web3(web3.currentProvider)
+    } else {
+        console.warn(
+            'No web3 detected. Falling back to http://127.0.0.1:9545.' +
+            ' You should remove this fallback when you deploy live, as it\'s inherently insecure.' +
+            ' Consider switching to Metamask for development.' +
+            ' More info here: http://truffleframework.com/tutorials/truffle-and-metamask'
+        )
+        // fallback - use your fallback strategy (local node / hosted node + in-dapp id mgmt / fail)
+        window.web3 = new Web3(new Web3.providers.HttpProvider('http://127.0.0.1:9545'))
+    }
 
-  App.start()
+    App.start()
 })
